0ecaf54ec6e6a18a6daabda8728a78fd2921054c
Vanessa Madu <vanessamadu197@gmail.com>
2022-08-26 19:16:35 +0100

    added functions looking for repetition and finding similarity scores

--- search_file.py
+++ search_file.py
@@ -1,5 +1,5 @@
 import json
-
+from data_structure import *
 #Load all modules
 with open('module_dict.json') as json_file:
     all_modules = json.load(json_file)
@@ -10,65 +10,106 @@ I need functions that do the following
 - Extracts required keywords from a module DONE
 - Extracts taught skills DONE
 - Extracts required skills DONE
-- Returns which section a keyword/skill is from
-- Is the keyword in the other list
+- Returns which section a keyword/skill is from DONE
+- Is the keyword in the other list DONE
 - Keeps track and count of what is repeated
 - Counting the number of skills
 '''
 ######################### EXTRACTION FUNCTIONS ##############################
 
-def taught_keywords(module_dict):
+def keywords(module_dict, index):
     '''
-    Extracts all of the taught keywords in a module where each 'keyword' is stored as a list.
+    Extracts all of the desired keywords/skills in a module where each 'keyword' is stored as a list.
     Parameters: 
         module_dict (dict): Module dictionary
+        index (int): Takes the values 1,2,3, or 4 to determine which list to 
+            extract
+            1: taught keywords
+            2: required keywords
+            3: taught skills
+            4: required skills
     Returns:
-        taught_keywords (list): List of taught keywords
+        keywords (list): List of desired keywords/skills
     '''
     num_of_sections = int(module_dict['module information'][4][0])
-    taught_keywords = []
+    keywords = []
     for i in range(1,num_of_sections+1):
-        taught_keywords+=(module_dict['section_'+str(i)][1])
-    return(taught_keywords)
+        keywords+=(module_dict['section_'+str(i)][index])
+    return(keywords)
 
-def required_keywords(module_dict):
-    '''
-    Extracts all of the prerequisite keywords in a module where each 'keyword' is stored as a list.
-    Parameters: 
-        module_dict (dict): Module dictionary
-    Returns:
-        required_keywords (list): List of prerequisite keywords
-    '''
-    num_of_sections = int(module_dict['module information'][4][0])
-    required_keywords = []
-    for i in range(1,num_of_sections+1):
-        required_keywords+=(module_dict['section_'+str(i)][2])
-    return(required_keywords)
+###################### HELPER FUNCTIONS ########################
 
-def taught_skills(module_dict):
+def is_repeated(module_dict, keyword, index):
     '''
-    Extracts all of the taught skills in a module where each 'skill' is stored as a list.
-    Parameters: 
+    Determines if a keyword/skill is in a module and if it is, returns where
+    Parameters:
         module_dict (dict): Module dictionary
-    Returns:
-        taught_skills (list): List of taught skills
+        keyword (str): Keyword/skill to be search for
+        index (int): Takes the values 1,2,3, or 4 to determine which list to 
+            search for the keyword/skill in:
+            1: taught keywords
+            2: required keywords
+            3: taught skills
+            4: required skills
+    Returns: 
+        Either: section_number (str): Section the keyword/skill is in
+        False (bool): False (the term is not in the list)
     '''
+    keyword = whitespace_cleaner(keyword)
     num_of_sections = int(module_dict['module information'][4][0])
-    taught_skills = []
-    for i in range(1,num_of_sections+1):
-        taught_skills+=(module_dict['section_'+str(i)][3])
-    return(taught_skills)
 
-def required_skills(module_dict):
-    '''
-    Extracts all of the required skills in a module where each 'skill' is stored as a list.
-    Parameters: 
-        module_dict (dict): Module dictionary
-    Returns:
-        required_skills (list): List of required skills
-    '''
-    num_of_sections = int(module_dict['module information'][4][0])
-    required_skills = []
     for i in range(1,num_of_sections+1):
-        required_skills+=(module_dict['section_'+str(i)][4])
-    return(required_skills)
+        # Specify which of the lists to search in
+        where_to_look = module_dict['section_'+str(i)][index]
+        for j in range(len(where_to_look)):
+            if keyword in where_to_look[j]:
+                return 'section_'+str(i)
+    return False
+
+########################## THE BIG FUNCTIONS ###############################
+
+def is_there_overlap(module_dict1, module_dict2, index1, index2):
+    '''
+    Finds repeated keywords between two modules, given the choice of which types of keywords are compared.
+    Module 1 is being compared to module 2.
+    Parameters:
+        module_dict1 (dict): Module dictionary 1
+        module_dict2 (dict): Module dictionary 2
+        index1 (int): Chooses which class of keywords/skills to be considered from module 1.
+        index2 (int): Chooses which class of keywords/skills to be considered from module 2.
+            1: taught keywords
+            2: required keywords
+            3: taught skills
+            4: required skills
+    returns:
+        similarity_list (list): List of unnormalised similarity scores in the form 
+        similarity_list = [number of repeated keywords, repeated keywords/number of chapters repeated across, max number of repeated
+        keywords in any section, squared sum of number of repeated keywords in each chapter]
+    '''
+    keywords_input = keywords(module_dict1,index1)
+    repeat_counter = 0
+    repeated_sections = {}
+
+    for i in range(len(keywords_input)):
+        if len(keywords_input[i]) == 1:
+            keyword = keywords_input[i][0]
+            section = is_repeated(module_dict2,keyword,index2)
+            if type(section) == str:
+                repeated_sections[section]+=1
+                repeat_counter+=1
+        else:
+            for j in range(len(keywords_input[i])):
+                keyword = keywords_input[i][j]
+                section = is_repeated(module_dict2,keyword,index2)
+                if type(section) == str:
+                    repeated_sections[section]+=1
+                    repeat_counter+=1
+                    break
+    squared_sum = 0
+    for k in range(len(repeated_sections.values())):
+        squared_sum += list(repeated_sections.values())[k]**2
+
+
+    return [repeat_counter, repeat_counter/len(repeated_sections.keys()), max(repeated_sections.values()), squared_sum]
+
+        
