2e6d9a72455e8d93e223929358a341c495784a17
Vanessa Madu <vanessamadu197@gmail.com>
2022-08-27 20:15:09 +0100

    update to do list

--- search_file.py
+++ search_file.py
@@ -15,6 +15,7 @@ I need functions that do the following
 - Keeps track and count of what is repeated DONE
 - Counting the number of skills DONE
 - Write a function that takes all the modules in the csv folder and loads them into the json file
+- Look for all pairs that have a metric higher than some value.
 '''
 ######################### EXTRACTION FUNCTIONS ##############################
 
