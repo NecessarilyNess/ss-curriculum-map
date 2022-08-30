3bcae12935d147eb4f2ca23e780129ef61953511
Vanessa Madu <vanessamadu197@gmail.com>
2022-08-30 13:36:05 +0100

    add doc strings

--- module_comparisons.py
+++ module_comparisons.py
@@ -1,135 +1,68 @@
 import pandas as pd
 from search_file import *
-from data_structure import *
+from helper_functions import *
 
-############################## HELPER FUNCTIONS ##################################
+########## COMPARISON FUNCTIONS NOT TO BE USED IN ISOLATION ####################
 
-def normalise(array, factor):
+def is_there_overlap(module_dict1, module_dict2, index1, index2):
     '''
-    Normalises all the values in symmetric array by a given factor 
+    Finds repeated keywords between two modules, given the choice of which types of keywords are compared.
+    Module 1 is being compared to module 2.
     Parameters:
-        array (array): Array of values
-        factor (int): Factor to normalise wrt
-    Returns:
-        array (array): Normalised array
+        module_dict1 (dict): Module dictionary 1
+        module_dict2 (dict): Module dictionary 2
+        index1 (int): Chooses which class of keywords/skills to be considered from module 1.
+        index2 (int): Chooses which class of keywords/skills to be considered from module 2.
+            1: taught keywords
+            2: required keywords
+            3: taught skills
+            4: required skills
+    returns:
+        similarity_list (list): List of unnormalised similarity scores in the form 
+        similarity_list = [number of repeated keywords, repeated keywords/number of chapters repeated across, max number of repeated
+        keywords in any section, squared sum of number of repeated keywords in each chapter]
     '''
-    for i in range(len(array)):
-                new_row = [val/factor for val in array[i]]
-                array[i] = new_row
-    return array
+    keywords_input = module_keywords(module_dict1,index1)
+    repeat_counter = 0
+    repeated_sections = {}
+    repeated_keywords = []
 
-def max_val_finder(array):
-    '''
-    Finds the maximum value in a symmetric array
-    Parameters: 
-        array (array): Array of values
-    Returns:
-        max_overall_value (int): Maximum value in the array
-    '''
-    max_overall_value = 0
-    for i in range(len(array)):
-        max_value = max(array[i])
-        if max_value > max_overall_value:
-            max_overall_value = max_value
-    return max_overall_value
+    for i in range(len(keywords_input)):
+        if len(keywords_input[i]) == 1:
+            keyword = keywords_input[i][0]
+            section = in_module(module_dict2,keyword,index2)
+            if type(section) == str:
+                if section in repeated_sections.keys():
+                    repeated_sections[section]+=1
+                else: 
+                    repeated_sections[section]=1
+                repeat_counter+=1
+                repeated_keywords.append(keyword)
+        else:
+            for j in range(len(keywords_input[i])):
+                keyword = keywords_input[i][j]
+                section = in_module(module_dict2,keyword,index2)
+                if type(section) == str:
+                    if section in repeated_sections.keys():
+                        repeated_sections[section]+=1
+                    else: 
+                        repeated_sections[section]=1
+                    repeat_counter+=1
+                    repeated_keywords.append(keyword)
+                    break
+    squared_sum = 0
+    for k in range(len(repeated_sections.values())):
+        squared_sum += list(repeated_sections.values())[k]**2
 
-def remove_diag(array):
-    '''
-    Sets the components on the diagonal of a symmetric array to zero.
-    Parameters:
-        array (array): Array of values
-    Returns:
-        array (array): The same array with the diagonal set to zero.
-    '''
-    for i in range(len(array)):
-        array[i][i] = 0
-    return array 
+    divided_repeats = 0
+    max_repeats = 0
+    if len(repeated_sections.keys()) != 0:
+        divided_repeats = repeat_counter/len(repeated_sections.keys())
+        max_repeats = max(repeated_sections.values())
 
-def pair_finder(all_modules,info_array, min_val):
-    '''
-    Look for all the module pairs that have a similarity metric greater than min_val
-    '''
-    pairs = []
-    module_codes = list(all_modules.keys())
-    length = len(info_array)
-    for i in range(length):
-        for j in range(length):
-            if info_array[i][j] > min_val:
-                pairs.append([module_codes[i],module_codes[j]])
-    return pairs
+    return [repeat_counter, divided_repeats , max_repeats, squared_sum, repeated_sections, repeated_keywords]
 
-def results(pairs, index1, index2):
-    '''
-    This only tells us about repeated keywords and shouldn't be used in isolation as index1 and index2 need to be the same as those
-    that were used to generate the info array.
-    '''
-    if index1 ==1 and index2==1:
-        pairs = remove_duplicate(pairs)
-        for i in range(len(pairs)):
-            module1 = pairs[i][0]
-            module2 = pairs[i][1]
-            print(' '.join([code_to_name(all_modules, module1)] + ['may be similar to'] + [code_to_name(all_modules, module2)]))
-    elif index1 == 1 and index2 == 2:
-        for i in range(len(pairs)):
-            module1 = pairs[i][0]
-            module2 = pairs[i][1]
-            print(' '.join([code_to_name(all_modules, module1)] + ['may be a good module to take for'] + [code_to_name(all_modules,module2)]))
-    elif index1 == 2 and index2 == 2:
-        pairs = remove_duplicate(pairs)
-        for i in range(len(pairs)):
-            module1 = pairs[i][0]
-            module2 = pairs[i][1]
-            print(' '.join([code_to_name(all_modules, module1)] + ["may have the same 'prerequisites' as"] + [code_to_name(all_modules, module2)]))
-    elif index1 == 2 and index2 == 1:
-        for i in range(len(pairs)):
-            module1 = pairs[i][0]
-            module2 = pairs[i][1]
-            print(' '.join([code_to_name(all_modules, module2)] + ['may be a good module to take for'] + [code_to_name(all_modules,module1)]))
-
-
-def clustering_results(pairs, index1, index2):
-    if index1 ==1 and index2==1:
-        pairs = remove_duplicate(pairs)
-        for i in range(len(pairs)):
-            module1 = pairs[i][0]
-            module2 = pairs[i][1]
-            print(' '.join(['The similarity in'] + [code_to_name(all_modules, module1)] + ['and'] + [code_to_name(all_modules, module2)] + ['is highly clustered in'] + [code_to_name(all_modules, module2)] ))
-    elif index1 == 1 and index2 == 2:
-        for i in range(len(pairs)):
-            module1 = pairs[i][0]
-            module2 = pairs[i][1]
-            print(' '.join(['The prerequisites for'] + [code_to_name(all_modules, module2)] + ['found in'] + [code_to_name(all_modules, module1)] + ['are highly clustered in'] + [code_to_name(all_modules, module2)] ))
-    elif index1 == 2 and index2 == 2:
-        pairs = remove_duplicate(pairs)
-        for i in range(len(pairs)):
-            module1 = pairs[i][0]
-            module2 = pairs[i][1]
-            print(' '.join(['The required keyword similarity in'] +[code_to_name(all_modules, module1)] + ['and'] + [code_to_name(all_modules, module2)] + ['is highly clustered in'] + [code_to_name(all_modules, module2)] ))
-    elif index1 == 2 and index2 == 1:
-            for i in range(len(pairs)):
-                module1 = pairs[i][0]
-                module2 = pairs[i][1]
-                print(' '.join(['The prerequisites for'] + [code_to_name(all_modules, module1)] + ['found in'] + [code_to_name(all_modules, module2)] + ['are highly clustered in'] + [code_to_name(all_modules, module2)] ))
-
-
-
-def remove_duplicate(pairs):
-    for i in range(len(pairs)):
-        pairs[i] = set(pairs[i])
-    for i in range(len(pairs)):
-        if pairs.count(pairs[i]) != 1:
-            pairs[i] = 'dup'
-    pairs = [list(pair) for pair in pairs if pair != 'dup']
-    return pairs
-
-def code_to_name(all_modules, code):
-    module = all_modules[code]
-    module_name = module["module information"][0][0]
-    return module_name
-
-###################################################################################
-
-
+## GENERATE INFORMATION ARRAYS
 def repeat_similarity(all_modules, index1, index2, info_index):
     '''
     Compares all modules available in module_dict.json for repeated keywords
@@ -172,19 +105,6 @@ def repeat_similarity(all_modules, index1, index2, info_index):
 
     return info_array
 
-def data_to_excel(all_modules, info_array):
-    '''
-    Writes chosen information into an excel spreadsheet
-    Parameters:
-        all_modules (dict): Dictionary containing all modules
-        info_array (array): Numerical metrics for each module
-    '''
-    module_codes = list(all_modules.keys())
-    df1 = pd.DataFrame(info_array,
-                    index=module_codes,
-                    columns=module_codes)
-    df1.to_excel(excel_writer = "/Users/vanessamadu/Documents/StudentShapers/StudentShapers_code/test.xlsx")
-
 def clustering_score(all_modules, index1, index2):
     '''
     Provides a table of relative scores of 'clustering' of repeated keywords across all modules. Weights divided repeats,
@@ -209,5 +129,137 @@ def clustering_score(all_modules, index1, index2):
         new_row = [(j+3*k+3*l)/7 for j,k,l in zip(divided_repeated_keywords[i], max_repeats[i], squared_sum[i])]
         clustering_matrix.append(new_row)
     return(clustering_matrix)
+
+## IDENTIFY MODULES WITH SIMILARITY ABOVE THRESHOLD
+def pair_finder(all_modules,info_array, min_val):
+    '''
+    Look for all the module pairs that have a similarity score greater than min_val
+    Parameters: 
+        all_modules (dict): Dictionary containing all modules
+        info_array (array): Either array containing normalised number of keywords or array containing
+                            the normalised clustering score.
+        min_val (float): Threshold value for two modules to be considered similar.
+    Returns: 
+        pairs (list): List of module pairs with similarity score above threshold.
+    '''
+    pairs = []
+    module_codes = list(all_modules.keys())
+    length = len(info_array)
+    for i in range(length):
+        for j in range(length):
+            if info_array[i][j] > min_val:
+                pairs.append([module_codes[i],module_codes[j]])
+    return pairs
+
+## PRINT USEFUL INFORMATION
+def keyword_repeat_results(pairs, index1, index2):
+    '''
+    Prints the results for pairs of modules that exceed the similarity score threshold, stating exactly how
+    **This only tells us about repeated keywords and shouldn't be used in isolation as index1 and index2 need to be the same as those
+    that were used to generate the info array.**
+    Parameters:
+        pairs (list): List of module pairs that exceed similarity score threshold
+        index1 (int): Takes the values 1 or 2
+        index2 (int): Takes the values 1 or 2
+            (1,1): Compares taught keywords 
+            (2,2): Compares required keywords
+            (1,2): Compares taught keywords in module 1 with required keywords in module 2.
+            (2,1): Compares required keywords in module 1 with taught keywords in module 2.
+    '''
+    if index1 ==1 and index2==1:
+        pairs = remove_duplicate(pairs)
+        for i in range(len(pairs)):
+            module1 = pairs[i][0]
+            module2 = pairs[i][1]
+            print(' '.join([code_to_name(all_modules, module1)] + ['may be similar to'] + [code_to_name(all_modules, module2)]))
+    elif index1 == 1 and index2 == 2:
+        for i in range(len(pairs)):
+            module1 = pairs[i][0]
+            module2 = pairs[i][1]
+            print(' '.join([code_to_name(all_modules, module1)] + ['may be a good module to take for'] + [code_to_name(all_modules,module2)]))
+    elif index1 == 2 and index2 == 2:
+        pairs = remove_duplicate(pairs)
+        for i in range(len(pairs)):
+            module1 = pairs[i][0]
+            module2 = pairs[i][1]
+            print(' '.join([code_to_name(all_modules, module1)] + ["may have the same 'prerequisites' as"] + [code_to_name(all_modules, module2)]))
+    elif index1 == 2 and index2 == 1:
+        for i in range(len(pairs)):
+            module1 = pairs[i][0]
+            module2 = pairs[i][1]
+            print(' '.join([code_to_name(all_modules, module2)] + ['may be a good module to take for'] + [code_to_name(all_modules,module1)]))
+
+def clustering_results(pairs, index1, index2):
+    '''
+    Prints the results for pairs of modules that exceed the clustering similarity score threshold, stating exactly how
+    **This only tells us about clustering of keywords and shouldn't be used in isolation as index1 and index2 need to be the same as those
+    that were used to generate the info array.**
+    Parameters:
+        pairs (list): List of module pairs that exceed similarity score threshold
+        index1 (int): Takes the values 1 or 2
+        index2 (int): Takes the values 1 or 2
+            (1,1): Compares taught keywords 
+            (2,2): Compares required keywords
+            (1,2): Compares taught keywords in module 1 with required keywords in module 2.
+            (2,1): Compares required keywords in module 1 with taught keywords in module 2.
+    '''
+    if index1 ==1 and index2==1:
+        pairs = remove_duplicate(pairs)
+        for i in range(len(pairs)):
+            module1 = pairs[i][0]
+            module2 = pairs[i][1]
+            print(' '.join(['The similarity in'] + [code_to_name(all_modules, module1)] + ['and'] + [code_to_name(all_modules, module2)] + ['is highly clustered in'] + [code_to_name(all_modules, module2)] ))
+    elif index1 == 1 and index2 == 2:
+        for i in range(len(pairs)):
+            module1 = pairs[i][0]
+            module2 = pairs[i][1]
+            print(' '.join(['The prerequisites for'] + [code_to_name(all_modules, module2)] + ['found in'] + [code_to_name(all_modules, module1)] + ['are highly clustered in'] + [code_to_name(all_modules, module2)] ))
+    elif index1 == 2 and index2 == 2:
+        pairs = remove_duplicate(pairs)
+        for i in range(len(pairs)):
+            module1 = pairs[i][0]
+            module2 = pairs[i][1]
+            print(' '.join(['The required keyword similarity in'] +[code_to_name(all_modules, module1)] + ['and'] + [code_to_name(all_modules, module2)] + ['is highly clustered in'] + [code_to_name(all_modules, module2)] ))
+    elif index1 == 2 and index2 == 1:
+            for i in range(len(pairs)):
+                module1 = pairs[i][0]
+                module2 = pairs[i][1]
+                print(' '.join(['The prerequisites for'] + [code_to_name(all_modules, module1)] + ['found in'] + [code_to_name(all_modules, module2)] + ['are highly clustered in'] + [code_to_name(all_modules, module2)] ))
+
+############################### COMPARE ALL MODULES TO EACH OTHER #########################################
+
+def similarity_all_modules(all_modules, index1, index2, repeat_or_cluster, min_val):
+    '''
+    Compares desired keywords across all modules for similarity, prints the modules that have a score above
+    the threshold and exports all the scores into a excel file.
+    Parameters:
+        all_modules (dict): Dictionary containing all the modules
+        index1 (int): Takes the values 1 or 2
+        index2 (int): Takes the values 1 or 2
+            (1,1): Compares taught keywords 
+            (2,2): Compares required keywords
+            (1,2): Compares taught keywords in module 1 with required keywords in module 2.
+            (2,1): Compares required keywords in module 1 with taught keywords in module 2.
+        repeat_or_cluster (int): Takes the values 1 or 2
+            1: Return results for repeated keywords
+            2: Return results for clustering of repeated keywords
+        min_val (float): Threshold value for two modules to be considered similar.
+    '''
+    if repeat_or_cluster == 1:
+        info_array = repeat_similarity(all_modules, index1, index2, 0)
+    elif repeat_or_cluster == 2:
+        info_array = clustering_score(all_modules, index1, index2)
+    
+    data_to_excel(all_modules, info_array)
+    pairs = pair_finder(all_modules,info_array, min_val)
+
+    if repeat_or_cluster == 1:
+        keyword_repeat_results(pairs, index1, index2)
+    elif repeat_or_cluster == 2: 
+        clustering_results(pairs, index1, index2)
+
+
+
+
 
 
